3baf971f8633040e732889b89ba75922a324c704
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-11-07 09:19:15 +0200

    Finished. It can consume a REST service

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"https://quoters.apps.pcfone.io/api/random",
+					Quote.class
+			);
+			log.info(quote.toString());
+		};
+	}
+}
